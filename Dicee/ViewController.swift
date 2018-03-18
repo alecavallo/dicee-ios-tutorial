5f44381d20f10c2711539a65e9d4915156ca2092
Pablo Gonzaga <pgonzaga.uy@gmail.com>
2018-03-18 09:51:51 -0300

    Changes left and right randomly

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var randomDiceIndexLeft : Int = 0
+    var randomDiceIndexRight : Int = 0
+
+    let dices : [String] = [
+        "dice1", "dice2", "dice3", "dice4", "dice5", "dice6"
+    ]
+
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +30,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
 
+        randomDiceIndexLeft = Int(arc4random_uniform(6))
+        randomDiceIndexRight = Int(arc4random_uniform(6))
+        
+        diceImageViewLeft.image = UIImage(
+            named: dices[randomDiceIndexLeft]
+        )
+        
+        diceImageViewRight.image = UIImage(
+            named: dices[randomDiceIndexRight]
+        )
+
+    }
+    
 }
 
